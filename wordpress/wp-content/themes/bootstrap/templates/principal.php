41387394bdcb21165fea6b0680f5b682a51147d8
lenincabezas <lenin_cabezas@outlook.com>
2020-10-07 08:37:57 -0500

    remover elementos inecesarios del template principal

--- wordpress/wp-content/themes/bootstrap/templates/principal.php
+++ wordpress/wp-content/themes/bootstrap/templates/principal.php
@@ -13,18 +13,6 @@ get_header();
 
 <main id="site-content" role="main">
 
-	<?php
-
-	if ( have_posts() ) {
-
-		while ( have_posts() ) {
-			the_post();
-
-			get_template_part( 'template-parts/content-cover' );
-		}
-	}
-
-	?>
 <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
