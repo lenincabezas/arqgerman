6cffa6e2f40fa0a54b4c9335bb5cd75dd70ad4eb
lenincabezas <lenin_cabezas@outlook.com>
2020-10-09 07:48:20 -0500

    creacion de nuevo temple para proyectos

---
+++ wordpress/wp-content/themes/bootstrap/templates/proyectos.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Template Name: proyectos
+ * Template Post Type: post, page
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty
+ * @since Twenty Twenty 1.0
+ */
+
+get_header();
+?>
+        <?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+
+<main id="site-content" role="main">
+    <div class="container">	
+            <div class="row">
+                <div class="col-sm"> 
+                    <h3>Nombre del proyecto</h3>
+                    <img src="https://i.pinimg.com/originals/96/ee/7d/96ee7de935580016f7d29b7d77368fd9.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-sm"> 
+                    <h3>Nombre del proyecto </h3>
+                    <img src="https://i.pinimg.com/originals/96/ee/7d/96ee7de935580016f7d29b7d77368fd9.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-sm"> 
+                    <h3> Nombre del proyecto </h3>
+                    <img src="https://i.pinimg.com/originals/96/ee/7d/96ee7de935580016f7d29b7d77368fd9.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+            </div>
+        <div class="container">	
+            <div class="row">
+                <div class="col-sm"> 
+                    <h3>Nombre del proyecto</h3>
+                    <img src="https://i.pinimg.com/originals/96/ee/7d/96ee7de935580016f7d29b7d77368fd9.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-sm"> 
+                    <h3>Nombre del proyecto </h3>
+                    <img src="https://i.pinimg.com/originals/96/ee/7d/96ee7de935580016f7d29b7d77368fd9.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-sm"> 
+                    <h3> Nombre del proyecto </h3>
+                    <img src="https://i.pinimg.com/originals/96/ee/7d/96ee7de935580016f7d29b7d77368fd9.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+            </div> 
+        </div>
+        <div class="container">	
+            <div class="row">
+                <div class="col-sm"> 
+                    <h3>Nombre del proyecto</h3>
+                    <img src="https://i.pinimg.com/originals/96/ee/7d/96ee7de935580016f7d29b7d77368fd9.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-sm"> 
+                    <h3>Nombre del proyecto </h3>
+                    <img src="https://i.pinimg.com/originals/96/ee/7d/96ee7de935580016f7d29b7d77368fd9.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-sm"> 
+                    <h3> Nombre del proyecto </h3>
+                    <img src="https://i.pinimg.com/originals/96/ee/7d/96ee7de935580016f7d29b7d77368fd9.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+            </div> 
+        </div>
+        <div class="container">	
+            <div class="row">
+                <div class="col-sm"> 
+                    <h3>Nombre del proyecto</h3>
+                    <img src="https://i.pinimg.com/originals/96/ee/7d/96ee7de935580016f7d29b7d77368fd9.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-sm"> 
+                    <h3>Nombre del proyecto </h3>
+                    <img src="https://i.pinimg.com/originals/96/ee/7d/96ee7de935580016f7d29b7d77368fd9.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+                <div class="col-sm"> 
+                    <h3> Nombre del proyecto </h3>
+                    <img src="https://i.pinimg.com/originals/96/ee/7d/96ee7de935580016f7d29b7d77368fd9.jpg" class="img-fluid" alt="Responsive image">
+                </div>
+            </div> 
+        </div> 
+
+</main><!-- #site-content -->
+
+<?php get_footer(); ?>
